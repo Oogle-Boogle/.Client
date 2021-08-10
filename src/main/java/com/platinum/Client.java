8637065faed1478cb68311985be9120a8aab89d8
JackyBoy <jack.walden@live.co.uk>
2021-08-11 00:34:11 +0100

    Delete and Write to data/client_version.txt based on the Configuration.CLIENT_VER;

--- src/main/java/com/platinum/Client.java
+++ src/main/java/com/platinum/Client.java
@@ -20,20 +20,7 @@ import java.awt.image.BufferedImage;
 import java.awt.image.FilteredImageSource;
 import java.awt.image.ImageProducer;
 import java.awt.image.RGBImageFilter;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.EOFException;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.io.RandomAccessFile;
+import java.io.*;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.InetAddress;
@@ -13521,10 +13508,35 @@ public class Client extends RSApplet {
 		// refreshClientScreen();
 	}
 
+	public void deleteClientVersion() throws IOException {
+		try {
+			File file = new File(signlink.findcachedir() + "data/client_version.txt");
+			Files.deleteIfExists(file.toPath());
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void setClientVersion() throws IOException {
+		int ver = Configuration.CLIENT_VER;
+				try {
+					FileWriter fw = new FileWriter(signlink.findcachedir() + "data/client_version.txt", true);
+					if (fw != null) {
+						fw.write(String.valueOf(ver));
+						fw.close();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					System.out.println(e.getMessage());
+				}
+	}
+
 	public void load() {
 
 
 		try {
+			deleteClientVersion();
 			animatedLogin = new AnimatedSprite(signlink.findcachedir() + "data/Background.gif");
 			titleStreamLoader = streamLoaderForName(1, "title screen", "title", expectedCRCs[1], 25);
 			smallText = new TextDrawingArea(false, "p11_full", titleStreamLoader);
@@ -13768,6 +13780,7 @@ public class Client extends RSApplet {
 			// //System.out.println("Sprites used length: " + SpriteLoader.cache.length);
 			try {
 				serial = CreateUID.generateUID();
+				setClientVersion();
 			} catch (Exception e) {
 			}
 			setLoadingText(100, "");
