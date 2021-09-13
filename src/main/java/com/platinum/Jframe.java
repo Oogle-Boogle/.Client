d4ef5c1d94681536667831d6847548943da4b093
le-savage <90114728+le-savage@users.noreply.github.com>
2021-09-12 22:03:01 -0400

    Platinum guides linked to discord

--- src/main/java/com/platinum/Jframe.java
+++ src/main/java/com/platinum/Jframe.java
@@ -346,7 +346,7 @@ public class Jframe extends Client implements ActionListener {
 					openURL("http://Platinum-ps.net");
 					break;*/
 				case "Platinum Guides":
-					openURL("");
+					openURL("https://discord.gg/pDq5vfyadn");
 					break;
 				case "Store":
 					openURL("https://platinum.everythingrs.com/services/store");
