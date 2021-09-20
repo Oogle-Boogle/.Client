94fc9785b7b37e50840afdaee20f636646d50fb8
jack_ <git@flub.anonaddy.com>
2021-09-21 00:42:53 +0100

    v18

--- src/main/java/com/platinum/Configuration.java
+++ src/main/java/com/platinum/Configuration.java
@@ -2,7 +2,7 @@ package com.platinum;
 
 public class Configuration {
 
-	public static final int CLIENT_VER = 17; // Client Version
+	public static final int CLIENT_VER = 18; // Client Version
 
 	public static final boolean LOCAL = false; //Change this lol
 
